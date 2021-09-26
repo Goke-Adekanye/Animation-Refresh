2840b5cc1605036345dbc906f9fd93f3799b7606
Goke Adekanye <adekanyeadegoke96@gmail.com>
2021-09-26 09:42:47 -0700

    fixed dropdown menu

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,29 +1,118 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import {
+  fadeInUp,
+  handleCity,
+  handleCityReturn,
+  handleHover,
+  handleHoverExit,
+  staggerReveal,
+  staggerRevealClose,
+  staggerText,
+} from "./Animations";
 
-const Hamburger = () => {
+import dallas from "../images/dallas.webp";
+import austin from "../images/austin.webp";
+import newyork from "../images/newyork.webp";
+import sanfrancisco from "../images/sanfrancisco.webp";
+import beijing from "../images/beijing.webp";
+
+const cities = [
+  { name: "Dallas", image: dallas },
+  { name: "Austin", image: austin },
+  { name: "New York", image: newyork },
+  { name: "San Francisco", image: sanfrancisco },
+  { name: "Beijing", image: beijing },
+];
+
+const Hamburger = ({ state }) => {
+  // Create varibles of our dom nodes
+  let menuLayer = useRef(null);
+  let reveal1 = useRef(null);
+  let reveal2 = useRef(null);
+  let cityBackground = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  let info = useRef(null);
+
+  useEffect(() => {
+    // If the menu is open and we click the menu button to close it.
+    if (state.clicked === false) {
+      // If menu is closed and we want to open it.
+
+      staggerRevealClose(reveal2, reveal1);
+      // Set menu to display none
+      gsap.to(menuLayer, { duration: 1, css: { display: "none" } });
+    } else if (
+      state.clicked === true ||
+      (state.clicked === true && state.initial === null)
+    ) {
+      // Set menu to display block
+      gsap.to(menuLayer, { duration: 0, css: { display: "block" } });
+      //Allow menu to have height of 100%
+      gsap.to([reveal1, reveal2], {
+        duration: 0,
+        opacity: 1,
+        height: "100%",
+      });
+
+      staggerReveal(reveal1, reveal2);
+      fadeInUp(info);
+      staggerText(line1, line2, line3);
+    }
+  }, [state]);
+
   return (
-    <div className="hamburger-menu">
-      <div className="menu-secondary-background-color"></div>
-      <div className="menu-layer">
-        <div className="menu-city-background"></div>
+    <div ref={(el) => (menuLayer = el)} className="hamburger-menu">
+      <div
+        ref={(el) => (reveal1 = el)}
+        className="menu-secondary-background-color"
+      ></div>
+      <div ref={(el) => (reveal2 = el)} className="menu-layer">
+        <div
+          ref={(el) => (cityBackground = el)}
+          className="menu-city-background"
+        ></div>
         <div className="container">
           <div className="wrapper">
             <div className="menu-links">
               <nav>
                 <ul>
                   <li>
-                    <Link to="/opportunities">Opportunities</Link>
+                    <Link
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line1 = el)}
+                      to="/opportunities"
+                    >
+                      Opportunities
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/solutions">Solutions</Link>
+                    <Link
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line2 = el)}
+                      to="/solutions"
+                    >
+                      Solutions
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/contact-us">Contact us</Link>
+                    <Link
+                      onMouseEnter={(e) => handleHover(e)}
+                      onMouseOut={(e) => handleHoverExit(e)}
+                      ref={(el) => (line3 = el)}
+                      to="/contact-us"
+                    >
+                      Contact us
+                    </Link>
                   </li>
                 </ul>
               </nav>
-              <div className="info">
+              <div ref={(el) => (info = el)} className="info">
                 <h3>Our Promise</h3>
                 <p>
                   The passage experienced a surge in popularity during the 1960s
@@ -34,11 +123,15 @@ const Hamburger = () => {
               </div>
               <div className="locations">
                 {/* Returning the list of cities */}
-                <span>Dallas</span>
-                <span>Dallas</span>
-                <span>Dallas</span>
-                <span>Dallas</span>
-                <span>Dallas</span>
+                {cities.map((el) => (
+                  <span
+                    key={el.name}
+                    onMouseEnter={() => handleCity(el.image, cityBackground)}
+                    onMouseOut={() => handleCityReturn(cityBackground)}
+                  >
+                    {el.name}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { withRouter, Link } from "react-router-dom";
 import Hamburger from "./Hamburger";
 
-const Header = () => {
+const Header = ({ history }) => {
+  // State of our Menu
+  const [state, setState] = useState({
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+  });
+
+  // State of our button
+  const [disabled, setDisabled] = useState(false);
+
+  //Use Effect
+  useEffect(() => {
+    //Listening for page changes.
+    history.listen(() => {
+      setState({ clicked: false, menuName: "Menu" });
+    });
+  }, [history]);
+
+  // Toggle menu
+  const handleMenu = () => {
+    disableMenu();
+    if (state.initial === false) {
+      setState({
+        initial: null,
+        clicked: true,
+        menuName: "Close",
+      });
+    } else if (state.clicked === true) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+    } else if (state.clicked === false) {
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+    }
+  };
+
+  //Determine if out menu button should be disabled
+  const disableMenu = () => {
+    setDisabled(!disabled);
+    setTimeout(() => {
+      setDisabled(false);
+    }, 1200);
+  };
+
   return (
     <header>
       <div className="container">
@@ -12,14 +60,16 @@ const Header = () => {
               <Link to="/">HAMBRG.</Link>
             </div>
             <div className="menu">
-              <button>Menu</button>
+              <button onClick={handleMenu} disabled={disabled}>
+                {state.menuName}
+              </button>
             </div>
           </div>
         </div>
       </div>
-      <Hamburger />
+      <Hamburger state={state} />
     </header>
   );
 };
 
-export default Header;
+export default withRouter(Header);
