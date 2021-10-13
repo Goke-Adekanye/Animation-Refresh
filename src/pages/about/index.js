c07d1f8502d0ab1185c4d027ba9d78da8f08232f
Goke Adekanye <adekanyeadegoke96@gmail.com>
2021-10-12 23:50:54 -0700

    fixing about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,12 +1,42 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function About() {
+  let node1 = useRef(null);
+  let node2 = useRef(null);
+  let node3 = useRef(null);
+  let node4 = useRef(null);
+  let node5 = useRef(null);
+  let node6 = useRef(null);
+  let node7 = useRef(null);
+  let node8 = useRef(null);
+  let node9 = useRef(null);
+
   useEffect(() => {
     window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".second",
+          start: "center bottom",
+        },
+      })
+      .from([node1, node2, node3, node4, node5, node6, node7, node8, node9], {
+        duration: 5,
+        opacity: 0,
+        y: -100,
+        delay: 0.1,
+        ease: "Expo.easeInOut",
+        stagger: {
+          amount: 1,
+        },
+      });
   }, []);
 
   useEffect(() => {
@@ -36,18 +66,36 @@ export default function About() {
           <div className="wrapper">
             <div className="text-desc">
               <h2>
-                <span>HALIMRT. is a full-service creative studio </span>
-                <span>working with fashion, tech & lifestyle brands. </span>
-                <span>We collaborate closely with our clients </span>
-                <span>to tell their stories, engaging audiences </span>
-                <span>by building beautiful experiences & products.</span>
+                <span ref={(el) => (node1 = el)}>
+                  HALIMRT. is a full-service creative studio{" "}
+                </span>
+                <span ref={(el) => (node2 = el)}>
+                  working with fashion, tech & lifestyle brands.{" "}
+                </span>
+                <span ref={(el) => (node3 = el)}>
+                  We collaborate closely with our clients{" "}
+                </span>
+                <span ref={(el) => (node4 = el)}>
+                  to tell their stories, engaging audiences{" "}
+                </span>
+                <span ref={(el) => (node5 = el)}>
+                  by building beautiful experiences & products.
+                </span>
               </h2>
 
               <h2 className="sec">
-                <span>We strongly believe every brand and project </span>
-                <span>is unique and should be tailor-made from </span>
-                <span>industry leaders to early stage companies. </span>
-                <span>We don’t do one size fits all.</span>
+                <span ref={(el) => (node6 = el)}>
+                  We strongly believe every brand and project{" "}
+                </span>
+                <span ref={(el) => (node7 = el)}>
+                  is unique and should be tailor-made from{" "}
+                </span>
+                <span ref={(el) => (node8 = el)}>
+                  industry leaders to early stage companies.{" "}
+                </span>
+                <span ref={(el) => (node9 = el)}>
+                  We don’t do one size fits all.
+                </span>
               </h2>
             </div>
           </div>
