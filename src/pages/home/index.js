de4486ef80857c3f1d0b9ab97b9c10a77819d60c
Goke Adekanye <adekanyeadegoke96@gmail.com>
2021-10-16 07:55:17 -0700

    fixing home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -20,6 +20,9 @@ export default function Home() {
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
   }, []);
 
   useEffect(() => {
