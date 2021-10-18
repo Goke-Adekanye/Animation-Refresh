88b4729d9f0595972d3a0e75d98d23274bcc2fd1
Goke Adekanye <adekanyeadegoke96@gmail.com>
2021-10-18 00:57:23 -0700

    fixing discover page mv

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,80 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BBi from "../../images/portfolio/Black-Bananas_2000x_img.jpg";
-import BBl from "../../images/portfolio/Blackbananas_2000x.png";
-import DCd from "../../images/portfolio/Danielle-Cathari-Dsk.jpg";
-import DCm from "../../images/portfolio/Danielle-Cathari_Mbl.jpg";
-import DCl from "../../images/portfolio/Danielle-cathari_Logo.png";
+
+import portfolioData from "./portfolioData";
 
 export default function Disover() {
   return (
     <div className="protfoliopage">
       <div className="portfolio_cls">
-        <div className="portfolio_grid">
-          <div className="portfolio_gridinr">
-            <Link to="/discover">
-              <img
-                srcSet={BBi}
-                sizes="100vw"
-                src={BBi}
-                loading="lazy"
-                alt="Daily Paper"
-                className="abs_img desktop_img"
-              />
-              <img
-                srcSet={BBi}
-                sizes="100vw"
-                src={BBi}
-                loading="lazy"
-                alt="Daily Paper"
-                className="abs_img mobile_img"
-              />
-              <div className="portfolio-logo">
-                <img
-                  srcset={BBl}
-                  sizes="100vw"
-                  src={BBl}
-                  loading="lazy"
-                  alt="blackbananas"
-                  className="blackbananas"
-                />
+        {portfolioData.work.map(
+          ({
+            srcSet_d,
+            src_d,
+            srcSet_m,
+            src_m,
+            alt,
+            srcSet_l,
+            src_l,
+            class_l,
+          }) => (
+            <div className="portfolio_grid">
+              <div className="portfolio_gridinr">
+                <Link to="/discover">
+                  <img
+                    srcSet={srcSet_d}
+                    // sizes="100vw"
+                    src={src_d}
+                    loading="lazy"
+                    alt={alt}
+                    className="abs_img desktop_img"
+                  />
+                  <img
+                    srcSet={srcSet_m}
+                    sizes="100vw"
+                    src={src_m}
+                    loading="lazy"
+                    alt={alt}
+                    className="abs_img mobile_img"
+                  />
+                  <div className="portfolio-logo">
+                    <img
+                      srcset={srcSet_l}
+                      sizes="100vw"
+                      src={src_l}
+                      loading="lazy"
+                      alt={alt}
+                      className={class_l}
+                    />
+                  </div>
+                </Link>
               </div>
-            </Link>
-          </div>
-        </div>
-
-        <div className="portfolio_grid">
-          <div className="portfolio_gridinr">
-            <Link to="/discover">
-              <img
-                srcSet={DCd}
-                sizes="100vw"
-                src={DCd}
-                loading="lazy"
-                alt="Daniëlle Cathari"
-                className="abs_img desktop_img"
-              />
-              <img
-                srcSet={DCm}
-                sizes="100vw"
-                src={DCm}
-                loading="lazy"
-                alt="Daniëlle Cathari"
-                className="abs_img mobile_img"
-              />
-              <div className="portfolio-logo">
-                <img
-                  srcset={DCl}
-                  sizes="100vw"
-                  src={DCl}
-                  loading="lazy"
-                  alt="daniëllecathari"
-                  className="daniellecathari"
-                />
-              </div>
-            </Link>
-          </div>
-        </div>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
---
+++ src/pages/discover/portfolioData.js
@@ -0,0 +1,134 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  work: [
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/209655800_989931288503095_655491104225370677_n_88762e7f-4d68-4f8a-a681-127a53caf3c6_2000x.jpg?v=1630421782 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/209655800_989931288503095_655491104225370677_n_88762e7f-4d68-4f8a-a681-127a53caf3c6_1900x.jpg?v=1630421782",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/209655800_989931288503095_655491104225370677_n_88762e7f-4d68-4f8a-a681-127a53caf3c6_1900x.jpg?v=1630421782",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/209655800_989931288503095_655491104225370677_n_88762e7f-4d68-4f8a-a681-127a53caf3c6_1900x.jpg?v=1630421782",
+      alt: "Daily Paper",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Dailypaper_510834af-bbda-4d9e-83a5-35253044fdb1_2000x.png?v=1630421481 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Dailypaper_510834af-bbda-4d9e-83a5-35253044fdb1_1900x.png?v=1630421481",
+      class_l: "dailypaper",
+    },
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Danielle-Cathari-Square_2000x.jpg?v=1630421711 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Danielle-Cathari-Square_1900x.jpg?v=1630421711",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Danielle-Cathari_2000x.jpg?v=1630421724 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Danielle-Cathari_1900x.jpg?v=1630421724",
+      alt: "Daniëlle Cathari",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Danielle-cathari_2000x.png?v=1630421509 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Danielle-cathari_1900x.png?v=1630421509",
+      class_l: "daniellecathari",
+    },
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Black-Bananas_2000x.jpg?v=1630421806 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Black-Bananas_1900x.jpg?v=1630421806",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Black-Bananas_2000x.jpg?v=1630421806 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Black-Bananas_1900x.jpg?v=1630421806",
+      alt: "Black Bananas",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Blackbananas_2000x.png?v=1630421525 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Blackbananas_1900x.png?v=1630421525",
+      class_l: "blackbananas",
+    },
+
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/ETQ-Square_2000x.jpg?v=1630421868 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/ETQ-Square_1900x.jpg?v=1630421868",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/ETQ_2000x.jpg?v=1630421871 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/ETQ_1900x.jpg?v=1630421871",
+      alt: "ETQ",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/ETQ_2000x.png?v=1630421557 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/ETQ_1900x.png?v=1630421557",
+      class_l: "etq",
+    },
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Mastoor-Square_d2b4df2e-d9b1-4e6f-95a8-599ac08c6195_2000x.jpg?v=1630424151 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Mastoor-Square_d2b4df2e-d9b1-4e6f-95a8-599ac08c6195_1900x.jpg?v=1630424151",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Mastoor_2000x.jpg?v=1630424123 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Mastoor_1900x.jpg?v=1630424123",
+      alt: "mastoor",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Mastoor_2000x.png?v=1631882499 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Mastoor_1900x.png?v=1631882499",
+      class_l: "mastoor",
+    },
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Maha_2000x.jpg?v=1630421843 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Maha_1900x.jpg?v=1630421843",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Maha_2000x.jpg?v=1630421843 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Maha_1900x.jpg?v=1630421843",
+      alt: "Maha Amsterdam",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Maha_2000x.png?v=1630406162 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Maha_1900x.png?v=1630406162",
+      class_l: "maha",
+    },
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Burker-Watches-Square_2000x.jpg?v=1630424373 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Burker-Watches-Square_1900x.jpg?v=1630424373",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Burker-Watches_2000x.jpg?v=1630424399 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Burker-Watches_1900x.jpg?v=1630424399",
+      alt: "Burker Watches",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Burker_2000x.png?v=1631882573 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Burker_1900x.png?v=1631882573",
+      class_l: "burker",
+    },
+    {
+      srcSet_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/CGM_5a9c4bd3-dff2-47c5-8ce4-97a954f7200e_2000x.jpg?v=1630429418 500w",
+      src_d:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/CGM_5a9c4bd3-dff2-47c5-8ce4-97a954f7200e_1900x.jpg?v=1630429418",
+      srcSet_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/CGM_5a9c4bd3-dff2-47c5-8ce4-97a954f7200e_2000x.jpg?v=1630429418 500w",
+      src_m:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/CGM_5a9c4bd3-dff2-47c5-8ce4-97a954f7200e_1900x.jpg?v=1630429418",
+      alt: "cgm",
+      srcSet_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Curlygirlmovement_2000x.png?v=1631882616 500w",
+      src_l:
+        "//cdn.shopify.com/s/files/1/0582/6189/7408/files/Curlygirlmovement_1900x.png?v=1631882616",
+      class_l: "cgm",
+    },
+  ],
+};
